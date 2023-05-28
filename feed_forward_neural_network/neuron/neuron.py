a40af775cdf50cf4ec3236e0939ffa00a9061d57
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2023-05-28 18:31:55 +0200

    pushing_neuron_structure

--- feed_forward_neural_network/neuron/neuron.py
+++ feed_forward_neural_network/neuron/neuron.py
@@ -1,4 +1,5 @@
 import torch
+from feed_forward_neural_network.activation.activation import sigmoid
 
 class neuron:
     """
@@ -10,10 +11,37 @@ class neuron:
         bias of the neuron
         -weight: torch.tensor: The
         weight of the neuron
+        -activation: str: The 
+        activation function of the
+        network
     Returns:
         -None
     """
     
-    def __init__(self, bias: torch.tensor, weight:torch.tensor):
+    def __init__(self, bias: torch.tensor, weight:torch.tensor, activation: str ="sigmoid"):
         self.bias=bias
         self.weight=weight
+        
+        if activation=="sigmoid":
+            self.activation=staticmethod(sigmoid).__func__
+        
+    def compute_output_value(self,input:torch.tensor)->torch.tensor:
+        """
+        The goal of this function is
+        to compute the output value
+        of a neuron once fed with the
+        input values
+        
+        Arguments:
+            -input: torch.tensor: The
+            input values of the neuron
+        Returns:
+            -output_value: torch.tensor:
+            The ouput value of the neuron
+        """
+
+        output_value=torch.matmul(self.weight,input)
+        output_value+=self.bias
+        output_value=self.activation(output_value)
+
+        return output_value
