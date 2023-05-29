c446c78e7d7fc72a0613cc208100a9d126e433a8
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2023-05-29 10:36:37 +0200

    pushing_layer_structure

--- feed_forward_neural_network/layer/layer.py
+++ feed_forward_neural_network/layer/layer.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 from feed_forward_neural_network.neuron.neuron import neuron
 
 class layer:
@@ -17,8 +18,14 @@ class layer:
         -None
     """
 
-    def __init__(self, input_size: int, hidden_size: int)->None:
+    def __init__(self, input_size: int, hidden_size: int, *args, **kwargs)->None:
         self.input_size=input_size
         self.hidden_size=hidden_size
-
+        self.layer_neurons=np.array([neuron(bias=x, weight=y) 
+                                     for x, y in zip(torch.randn(
+                                    size=(self.input_size,1)),
+                                    torch.randn(size=(self.input_size,1)))])
+        self.biases=torch.tensor([neuron.bias for neuron in self.layer_neurons])
+        self.weights=torch.tensor([neuron.weight for neuron in self.layer_neurons])
         
+   
