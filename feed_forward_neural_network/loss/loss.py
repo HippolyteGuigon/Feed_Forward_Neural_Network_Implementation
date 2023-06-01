599ef4f7bbf753b040980a40255b36691c10f3df
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2023-06-01 16:51:57 +0200

    pushing_cross_categorical_loss_function

---
+++ feed_forward_neural_network/loss/loss.py
@@ -0,0 +1,21 @@
+import torch
+
+def categorical_cross_entropy(y_pred: torch.tensor, y_true:torch.tensor):
+    """
+    The goal of this function
+    is to compute the loss of
+    a prediction regarding ground 
+    truth witth respct to categorical
+    cross-entropy function
+    
+    Arguments:
+        -y_pred: torch.tensor: The prediction
+        made by the model
+        -y_true: torch.tensor: The true values
+    Returns:
+        -loss: torch.tensor(float): The computed
+        loss     
+    """
+
+    loss=-(y_true.float()@torch.log2(y_pred))
+    return loss
