c366d35448a0ae404af5b1167628e223f555ff4f
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2023-06-01 18:41:18 +0200

    pushing_unittest

---
+++ test_unnitest.py
@@ -0,0 +1,22 @@
+import unittest
+
+
+class Test(unittest.TestCase):
+    """
+    The goal of this class is to implement unnitest
+    and check everything commited makes sense
+    """
+
+    def test_single_forward_batch(self) -> None:
+        """
+        The goal of this function
+        is to check the availability
+        of the network to"""
+        pass
+
+    def test_loss_computation(self) -> None:
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
