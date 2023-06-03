79ad3ae5e6f86e50e85c88b11e954e30a87e5ba9
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2023-06-03 17:15:09 +0200

    pushing_test_function

---
+++ feed_forward_neural_network/test/test.py
@@ -0,0 +1,21 @@
+import torch
+import numpy as np
+
+def get_fake_label(x: int)->torch.tensor:
+    """
+    The goal of this function
+    is to generate fake labels
+    for the try of the neural 
+    network on the MNIST dataset
+    
+    Arguments:
+        -x: int: The number that's
+        represented on a given image
+    Returns:
+        -labels: torch.tensor: The tensor
+        containing binary position of the 
+        given number
+    """
+    labels=torch.zeros(size=(10,))
+    labels[x]=1
+    return np.array(labels)
