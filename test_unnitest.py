738ac6a1bab383ca2e28e01a388c5083322ecc30
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2023-06-04 10:08:38 +0200

    pushing_new_test

--- test_unnitest.py
+++ test_unnitest.py
@@ -6,6 +6,7 @@ from feed_forward_neural_network.neural_network.neural_network import neural_net
 from feed_forward_neural_network.test.test import get_label
 from feed_forward_neural_network.layer.layer import layer
 
+
 class Test(unittest.TestCase):
     """
     The goal of this class is to implement unnitest
@@ -16,9 +17,9 @@ class Test(unittest.TestCase):
         """
         The goal of this function
         is to check the availability
-        of the network to deal with 
+        of the network to deal with
         multiple size batches
-        
+
         Arguments:
             -None
         Returns:
@@ -27,33 +28,72 @@ class Test(unittest.TestCase):
 
         batch_size = 64
         train_loader = torch.utils.data.DataLoader(
-        torchvision.datasets.MNIST('data/', train=True, download=True,
-                             transform=torchvision.transforms.Compose([
-                               torchvision.transforms.ToTensor()])),
-        batch_size=batch_size, shuffle=True)
-        data_iterator=iter(train_loader)
-        batch=next(data_iterator)
-        
-        targets=batch[-1]
-        data=torch.stack([torch.flatten(x) for x in batch[0].squeeze()]).T
-        targets=torch.tensor([get_label(x) for x in targets])
-        network=neural_network(input_data=data,
-                    targets=targets)
-        layer_1=layer(64, 784, first_layer=True)
-        layer_2=layer(784,16,first_layer=False)
-        layer_3=layer(16,16, first_layer=False)
-        layer_4=layer(16,10,last_layer=True)
-        
-        network.forward(layer_1,layer_2)
-        network.forward(layer_2,layer_3)
-        network.forward(layer_3,layer_4)
+            torchvision.datasets.MNIST(
+                "data/",
+                train=True,
+                download=True,
+                transform=torchvision.transforms.Compose(
+                    [torchvision.transforms.ToTensor()]
+                ),
+            ),
+            batch_size=batch_size,
+            shuffle=True,
+        )
+        data_iterator = iter(train_loader)
+        batch = next(data_iterator)
+
+        targets = batch[-1]
+        data = torch.stack([torch.flatten(x) for x in batch[0].squeeze()]).T
+        targets = torch.tensor([get_label(x) for x in targets])
+        network = neural_network(input_data=data, targets=targets)
+        layer_1 = layer(64, 784, first_layer=True)
+        layer_2 = layer(784, 16, first_layer=False)
+        layer_3 = layer(16, 16, first_layer=False)
+        layer_4 = layer(16, 10, last_layer=True)
+
+        network.forward(layer_1, layer_2)
+        network.forward(layer_2, layer_3)
+        network.forward(layer_3, layer_4)
         prob, pred = network.output(layer_4)
-        
-        self.assertTrue(np.all(np.array([np.round(torch.sum(a).item(),5) for a in prob])==1))
-        self.assertEquals(pred.size()[0],batch_size)
+
+        self.assertTrue(
+            np.all(np.array([np.round(torch.sum(a).item(), 5) for a in prob]) == 1)
+        )
+        self.assertEquals(pred.size()[0], batch_size)
 
     def test_loss_computation(self) -> None:
-        pass
+        batch_size = 64
+        train_loader = torch.utils.data.DataLoader(
+            torchvision.datasets.MNIST(
+                "data/",
+                train=True,
+                download=True,
+                transform=torchvision.transforms.Compose(
+                    [torchvision.transforms.ToTensor()]
+                ),
+            ),
+            batch_size=batch_size,
+            shuffle=True,
+        )
+        data_iterator = iter(train_loader)
+        batch = next(data_iterator)
+
+        targets = batch[-1]
+        data = torch.stack([torch.flatten(x) for x in batch[0].squeeze()]).T
+        targets = torch.tensor([get_label(x) for x in targets])
+        network = neural_network(input_data=data, targets=targets)
+        layer_1 = layer(64, 784, first_layer=True)
+        layer_2 = layer(784, 16, first_layer=False)
+        layer_3 = layer(16, 16, first_layer=False)
+        layer_4 = layer(16, 10, last_layer=True)
+
+        network.forward(layer_1, layer_2)
+        network.forward(layer_2, layer_3)
+        network.forward(layer_3, layer_4)
+        prob, pred = network.output(layer_4)
+        loss = network.loss_compute(layer_4, targets)
+
+        self.assertGreaterEqual(loss, 0)
 
 
 if __name__ == "__main__":
