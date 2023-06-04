f2ce5e82dadb63992073a4622cf403e1287e81eb
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2023-06-04 10:05:08 +0200

    pushing_rmse

--- feed_forward_neural_network/loss/loss.py
+++ feed_forward_neural_network/loss/loss.py
@@ -1,22 +1,47 @@
 import torch
 
-def categorical_cross_entropy(y_pred: torch.tensor, y_true:torch.tensor):
+
+def categorical_cross_entropy(
+    y_pred: torch.tensor, y_true: torch.tensor
+) -> torch.tensor:
     """
     The goal of this function
     is to compute the loss of
-    a prediction regarding ground 
+    a prediction regarding ground
     truth witth respct to categorical
     cross-entropy function
-    
+
     Arguments:
         -y_pred: torch.tensor: The prediction
         made by the model
         -y_true: torch.tensor: The true values
     Returns:
         -loss: torch.tensor(float): The computed
-        loss     
+        loss
     """
     epsilon = 1e-30
     y_pred = torch.clamp(y_pred, epsilon, 1.0)
-    loss=-(y_true.float().T@torch.log2(y_pred))
+    loss = -(y_true.float().T @ torch.log2(y_pred))
+    return loss
+
+
+def rmse(y_pred: torch.tensor, y_true: torch.tensor) -> torch.tensor:
+    """
+    The goal of this function
+    is to compute the loss of
+    a prediction regarding ground
+    truth witth respct to categorical
+    cross-entropy function
+
+    Arguments:
+        -y_pred: torch.tensor: The prediction
+        made by the model
+        -y_true: torch.tensor: The true values
+    Returns:
+        -loss: torch.tensor(float): The computed
+        loss
+    """
+
+    loss = torch.sum(torch.square(y_true - y_pred))
+    loss = torch.sqrt(loss)
     return loss
