cf2e891eac4d54737d1d79a69d46b020edface64
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2023-06-25 16:32:13 +0200

    pushing_main

--- main.py
+++ main.py
@@ -1,24 +1,73 @@
 import argparse
 import logging
 import torch
+import torchvision
+from typing import List, Generator
 from feed_forward_neural_network.neural_network.neural_network import neural_network
+from feed_forward_neural_network.logs.logs import main
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(message)s")
 
+main()
+
 parser = argparse.ArgumentParser()
 
 parser.add_argument(
-    "number_epochs",
+    "--number_epochs",
     help="The number of epochs that\
                         will be used to train the model",
     default=5,
+    type=int,
 )
 
-parser.add_argument("batch_size", help="The batch size for each", default=200)
+parser.add_argument(
+    "--batch_size", help="The batch size for each", default=200, type=int
+)
 
 args = parser.parse_args()
+
+
+def get_loader() -> List[Generator]:
+    """
+    The goal of this function
+    is to get the train and test
+    loader for the MNIST dataset
+
+    Arguments:
+        -None
+    Returns:
+        -None
+    """
+
+    train_loader = torch.utils.data.DataLoader(
+        torchvision.datasets.MNIST(
+            "data/",
+            train=True,
+            download=True,
+            transform=torchvision.transforms.Compose(
+                [torchvision.transforms.ToTensor()]
+            ),
+        ),
+        batch_size=60000,
+        shuffle=True,
+    )
+
+    test_loader = torch.utils.data.DataLoader(
+        torchvision.datasets.MNIST(
+            "data/",
+            train=False,
+            download=True,
+            transform=torchvision.transforms.Compose(
+                [torchvision.transforms.ToTensor()]
+            ),
+        ),
+        batch_size=10000,
+        shuffle=True,
+    )
+
+    return train_loader, test_loader
 
 
 def launch_mnist_pepeline(
@@ -38,6 +87,9 @@ def launch_mnist_pepeline(
     Returns:
         -synthesis: str:
     """
+
+    train_loader, test_loader = get_loader()
+    logging.info("Train and test data successfully loaded !")
 
 
 if __name__ == "__main__":
