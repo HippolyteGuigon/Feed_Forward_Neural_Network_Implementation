469698ef902316bb2756e94ee5e1af485d8c9720
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2023-07-13 12:30:24 +0200

    pushing_corrected_regularization_syntax

--- feed_forward_neural_network/regularisation/regularisation.py
+++ feed_forward_neural_network/regularisation/regularisation.py
@@ -1,8 +1,8 @@
 import torch
 
-def lasso_regularization(x: torch.tensor, lambda: float)->float:
-    return  lambda*torch.norm(x, p=1)
+def lasso_regularization(x: torch.tensor, lambda_coefficient: float)->float:
+    return  lambda_coefficient*torch.norm(x, p=1)
 
-def ridge_regularization(x: torch.tensor, lambda: float)->float:
-    return  lambda*torch.norm(x, p=2)
+def ridge_regularization(x: torch.tensor, mu_coefficient: float)->float:
+    return  mu_coefficient*torch.norm(x, p=2)
 
