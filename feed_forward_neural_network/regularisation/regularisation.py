6f88bb38a851166bed7d1a4eb30994e88b7721b2
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2023-07-13 14:26:49 +0200

    pushing_further_arguments

--- feed_forward_neural_network/regularisation/regularisation.py
+++ feed_forward_neural_network/regularisation/regularisation.py
@@ -1,6 +1,6 @@
 import torch
 
-def lasso_regularization(x: torch.tensor, lambda_coefficient: float=1)->float:
+def lasso_regularization(x: torch.tensor, layer_list, lambda_coefficient: float=1)->float:
     """
     The goal of this function is
     to calculate the L1 Lasso penalization
@@ -10,6 +10,8 @@ def lasso_regularization(x: torch.tensor, lambda_coefficient: float=1)->float:
     Arguments:
         -x: torch.tensor: The weight tensor
         of the neural network
+        -layer_list: Class: The layer_list
+        from which the weights will be retrieved
         -lambda_coefficient: float: The L1
         lambda regularization coefficient
     Returns:
@@ -20,7 +22,7 @@ def lasso_regularization(x: torch.tensor, lambda_coefficient: float=1)->float:
     penalization=lambda_coefficient*torch.norm(x, p=1)
     return  penalization
 
-def ridge_regularization(x: torch.tensor, mu_coefficient: float=1)->float:
+def ridge_regularization(x: torch.tensor, layer_list, mu_coefficient: float=1)->float:
     """
     The goal of this function is
     to calculate the L2 Ridge penalization
@@ -30,6 +32,8 @@ def ridge_regularization(x: torch.tensor, mu_coefficient: float=1)->float:
     Arguments:
         -x: torch.tensor: The weight tensor
         of the neural network
+        -layer_list: Class: The layer_list
+        from which the weights will be retrieved
         -mu_coefficient: float: The L2
         mu regularization coefficient
     Returns:
@@ -40,7 +44,7 @@ def ridge_regularization(x: torch.tensor, mu_coefficient: float=1)->float:
     penalization=mu_coefficient*torch.norm(x, p=2)
     return  mu_coefficient*torch.norm(x, p=2)
 
-def elastic_net_regularization(x: torch.tensor, alpha: float)->float:
+def elastic_net_regularization(x: torch.tensor, layer_list, alpha: float)->float:
     """
     The goal of this function is
     to calculate the Elastic-net
@@ -50,6 +54,8 @@ def elastic_net_regularization(x: torch.tensor, alpha: float)->float:
     Arguments:
         -x: torch.tensor: The weight tensor
         of the neural network
+        -layer_list: Class: The layer_list
+        from which the weights will be retrieved
         -alpha: float: The elastic-net
         alpha regularization coefficient
     Returns:
