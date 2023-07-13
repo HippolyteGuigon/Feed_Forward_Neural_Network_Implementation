c6fca059782c7d747adad7df684dd6e4282bf661
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2023-07-13 12:36:07 +0200

    pushing_documented_regularization

--- feed_forward_neural_network/regularisation/regularisation.py
+++ feed_forward_neural_network/regularisation/regularisation.py
@@ -1,8 +1,42 @@
 import torch
 
 def lasso_regularization(x: torch.tensor, lambda_coefficient: float)->float:
-    return  lambda_coefficient*torch.norm(x, p=1)
+    """
+    The goal of this function is
+    to calculate the L1 Lasso penalization
+    deduced from the computed weights
+    during the neural network training
+    
+    Arguments:
+        -x: torch.tensor: The weight tensor
+        of the neural network
+        -lambda_coefficient: float: The L1
+        lambda regularization coefficient
+    Returns:
+        -penalization: float: The computed
+        penalization
+    """
+    
+    penalization=lambda_coefficient*torch.norm(x, p=1)
+    return  penalization
 
 def ridge_regularization(x: torch.tensor, mu_coefficient: float)->float:
+    """
+    The goal of this function is
+    to calculate the L2 Ridge penalization
+    deduced from the computed weights
+    during the neural network training
+    
+    Arguments:
+        -x: torch.tensor: The weight tensor
+        of the neural network
+        -mu_coefficient: float: The L2
+        mu regularization coefficient
+    Returns:
+        -penalization: float: The computed
+        penalization
+    """
+    
+    penalization=mu_coefficient*torch.norm(x, p=2)
     return  mu_coefficient*torch.norm(x, p=2)
 
