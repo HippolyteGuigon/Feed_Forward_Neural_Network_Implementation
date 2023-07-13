4b95b18604a9b4fce335cde1ae930e9c4eeb6461
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2023-07-13 12:39:45 +0200

    pushing_elastic-net

--- feed_forward_neural_network/regularisation/regularisation.py
+++ feed_forward_neural_network/regularisation/regularisation.py
@@ -1,6 +1,6 @@
 import torch
 
-def lasso_regularization(x: torch.tensor, lambda_coefficient: float)->float:
+def lasso_regularization(x: torch.tensor, lambda_coefficient: float=1)->float:
     """
     The goal of this function is
     to calculate the L1 Lasso penalization
@@ -20,7 +20,7 @@ def lasso_regularization(x: torch.tensor, lambda_coefficient: float)->float:
     penalization=lambda_coefficient*torch.norm(x, p=1)
     return  penalization
 
-def ridge_regularization(x: torch.tensor, mu_coefficient: float)->float:
+def ridge_regularization(x: torch.tensor, mu_coefficient: float=1)->float:
     """
     The goal of this function is
     to calculate the L2 Ridge penalization
@@ -36,7 +36,26 @@ def ridge_regularization(x: torch.tensor, mu_coefficient: float)->float:
         -penalization: float: The computed
         penalization
     """
-    
+
     penalization=mu_coefficient*torch.norm(x, p=2)
     return  mu_coefficient*torch.norm(x, p=2)
 
+def elastic_net_regularization(x: torch.tensor, alpha: float)->float:
+    """
+    The goal of this function is
+    to calculate the Elastic-net
+    deduced from the computed weights
+    during the neural network training
+    
+    Arguments:
+        -x: torch.tensor: The weight tensor
+        of the neural network
+        -alpha: float: The elastic-net
+        alpha regularization coefficient
+    Returns:
+        -penalization: float: The computed
+        penalization
+    """
+
+    penalization=alpha*lasso_regularization(x) + (1-alpha)*ridge_regularization(x)
+    return penalization
